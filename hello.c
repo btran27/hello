f9dba8c8cd698f9978b0ecdb00728e81e6ef1924
Brian <btran19@husker.unl.edu>
2021-08-26 17:42:21 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Brian Tran & Sean Grafton
+ *         btran19@huskers.unl.edu
+ *        sgrafton2@husker.unl.edu
+ * Date: 2021/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Brian & Sean\n");
+
+  return 0;
+}
